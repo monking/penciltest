5efe8712e2677806061a8f4a014888867936b31d
Christopher Lovejoy <c@lovejoy.info>
2014-05-04 01:20:59 -0700

    drawing with mouse or touch

--- src/coffee/penciltest.coffee
+++ src/coffee/penciltest.coffee
@@ -13,11 +13,60 @@ class PencilTest
     @container = @options.container or document.querySelector @options.containerSelector
     @container.className = 'penciltest-app'
 
+    @strokes = []
+    @lift()
     @buildContainer()
+    window.penciltest = @
 
   buildContainer: ->
-    markup = '<svg class="field"></svg>'
+    self = @
+    markup = '<div class="field"></div>'
 
     @container.innerHTML = markup
 
-    @field = @container.querySelector '.field'
+    @fieldElement = @container.querySelector '.field'
+    @field = new Raphael @fieldElement
+
+    mouseDownHandler = (event) ->
+      self.fieldElement.addEventListener 'mousemove', mouseMoveHandler
+      self.fieldElement.addEventListener 'touchmove', mouseMoveHandler
+
+    mouseMoveHandler = (event) ->
+      if event.type is 'touchmove'
+        x = event.touches[0].pageX - self.fieldElement.offsetLeft
+        y = event.touches[0].pageY - self.fieldElement.offsetTop
+      else
+        self.mark event.offsetX, event.offsetY
+
+      self.mark x, y
+      event.preventDefault()
+      false
+
+    mouseUpHandler = (event) ->
+      self.fieldElement.removeEventListener 'mousemove', mouseMoveHandler
+      self.fieldElement.removeEventListener 'touchmove', mouseMoveHandler
+      self.lift()
+
+    @fieldElement.addEventListener 'mousedown', mouseDownHandler
+    @fieldElement.addEventListener 'touchstart', mouseDownHandler
+    @fieldElement.addEventListener 'mouseup', mouseUpHandler
+    @fieldElement.addEventListener 'touchend', mouseUpHandler
+
+  getCurrentStroke: ->
+    @currentStrokeIndex isnt null and @strokes[@currentStrokeIndex]
+
+  mark: (x,y) ->
+    if @currentStrokeIndex is null
+      @currentStrokeIndex = @strokes.length
+      @strokes.push "M#{x} #{y}"
+    else
+      @strokes[@currentStrokeIndex] += "L#{x} #{y}"
+
+    @display()
+
+  display: ->
+    @field.clear()
+    @field.path stroke for stroke in @strokes
+
+  lift: ->
+    @currentStrokeIndex = null
--- src/penciltest.js
+++ src/penciltest.js
@@ -20,14 +20,74 @@ PencilTest = (function() {
     }
     this.container = this.options.container || document.querySelector(this.options.containerSelector);
     this.container.className = 'penciltest-app';
+    this.strokes = [];
+    this.lift();
     this.buildContainer();
+    window.penciltest = this;
   }
 
   PencilTest.prototype.buildContainer = function() {
-    var markup;
-    markup = '<svg class="field"></svg>';
+    var markup, mouseDownHandler, mouseMoveHandler, mouseUpHandler, self;
+    self = this;
+    markup = '<div class="field"></div>';
     this.container.innerHTML = markup;
-    return this.field = this.container.querySelector('.field');
+    this.fieldElement = this.container.querySelector('.field');
+    this.field = new Raphael(this.fieldElement);
+    mouseDownHandler = function(event) {
+      self.fieldElement.addEventListener('mousemove', mouseMoveHandler);
+      return self.fieldElement.addEventListener('touchmove', mouseMoveHandler);
+    };
+    mouseMoveHandler = function(event) {
+      var x, y;
+      if (event.type === 'touchmove') {
+        x = event.touches[0].pageX - self.fieldElement.offsetLeft;
+        y = event.touches[0].pageY - self.fieldElement.offsetTop;
+      } else {
+        self.mark(event.offsetX, event.offsetY);
+      }
+      self.mark(x, y);
+      event.preventDefault();
+      return false;
+    };
+    mouseUpHandler = function(event) {
+      self.fieldElement.removeEventListener('mousemove', mouseMoveHandler);
+      self.fieldElement.removeEventListener('touchmove', mouseMoveHandler);
+      return self.lift();
+    };
+    this.fieldElement.addEventListener('mousedown', mouseDownHandler);
+    this.fieldElement.addEventListener('touchstart', mouseDownHandler);
+    this.fieldElement.addEventListener('mouseup', mouseUpHandler);
+    return this.fieldElement.addEventListener('touchend', mouseUpHandler);
+  };
+
+  PencilTest.prototype.getCurrentStroke = function() {
+    return this.currentStrokeIndex !== null && this.strokes[this.currentStrokeIndex];
+  };
+
+  PencilTest.prototype.mark = function(x, y) {
+    if (this.currentStrokeIndex === null) {
+      this.currentStrokeIndex = this.strokes.length;
+      this.strokes.push("M" + x + " " + y);
+    } else {
+      this.strokes[this.currentStrokeIndex] += "L" + x + " " + y;
+    }
+    return this.display();
+  };
+
+  PencilTest.prototype.display = function() {
+    var stroke, _i, _len, _ref, _results;
+    this.field.clear();
+    _ref = this.strokes;
+    _results = [];
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      stroke = _ref[_i];
+      _results.push(this.field.path(stroke));
+    }
+    return _results;
+  };
+
+  PencilTest.prototype.lift = function() {
+    return this.currentStrokeIndex = null;
   };
 
   return PencilTest;
